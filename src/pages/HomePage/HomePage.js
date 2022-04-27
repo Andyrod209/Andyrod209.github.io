6ab1fb11defcae8b8fff5712a5bef476158d8d7f
andyrod <andyrod209@gmail.com>
2022-04-26 17:16:03 -0700

    Chore: finished home page HTML

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,10 +7,12 @@ const HomePage = () => {
         <div className="home">
           <h1>Hello! I'm a <u>Full Stack Developer!</u></h1>
             <img className="image-of-andrew" src={process.env.PUBLIC_URL+"images/pic-of-andrew-rodriguez.jpg"} 
-                alt ="Picture of Andrew Rodriguez"
-                style={{width: "40%"}}/>
+                alt ="Picture of Andrew Rodriguez"/>
                 
             <p class="home-text">My name is Andrew Rodriguez and I am a software developer!</p>
+
+            <button href={process.env.PUBLIC_URL+"extraFiles/Rodriguez_Resume.pdf"}>Resume</button>
+            <button>Projects</button>
         </div> );
 }
  
