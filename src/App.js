4c6163b4a8e222bdca469613175661f9502de7e5
Andrew Rodriguez <andyrod209@gmail.com>
2022-04-27 23:09:43 -0500

    Chore: added a projects folder and made it a flexbox style to have content easily spaced around.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar/Navbar';
 import HomePage from './pages/HomePage/HomePage.js';
 import About from './pages/About/About.js';
 import { Routes, Route, } from "react-router-dom";
+import Projects from './pages/Projects/Projects.js';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </div>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
             <ul>
                 <li ><a href='/'>AR</a></li>
                 <li ><a href="/about">About</a></li>
-                <li ><a>Projects</a></li>
+                <li ><a href='/projects'>Projects</a></li>
                 <li ><a>Contact me</a></li>
             </ul>
         </div> );
