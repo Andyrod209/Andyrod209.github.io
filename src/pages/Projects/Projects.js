b6bc27f0d60eb6a06b8fdb5fe81a67f630e0fcaa
andyrod <andyrod209@gmail.com>
2022-05-02 20:31:19 -0700

    Chore: Added pictures and aligned each project. Next... Title and summary of each project. checkout user stories.

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,38 +1,38 @@
 import './projects.css'
 const Projects = () => {
     return ( 
-            <><p>projects page</p>
-            <div className="projects">
+        <><h1 className='projects-title'>projects page</h1>
+        <div className="projects">
             <div class="card">
-                <img style={{ width: '100%' }} />
+                <img src={process.env.PUBLIC_URL + "images/spacey.png"}/>
                 <div class="container">
                     <h4><b>John Doe</b></h4>
                     <p>Architect Engineer</p>
                 </div>
             </div>
             <div class="card">
-                <img style={{ width: '100%' }} />
+                <img src={process.env.PUBLIC_URL + "images/meTube.png"}/>
                 <div class="container">
                     <h4><b>John Doe</b></h4>
                     <p>Architect Engineer</p>
                 </div>
             </div>
             <div class="card">
-                <img style={{ width: '100%' }} />
+                <img src={process.env.PUBLIC_URL + "images/robots-vs-dinosaurs.png"}/>
                 <div class="container">
                     <h4><b>John Doe</b></h4>
                     <p>Architect Engineer</p>
                 </div>
             </div>
             <div class="card">
-                <img style={{ width: '100%' }} />
+                <img src={process.env.PUBLIC_URL + "images/day-trip-generator.png"} />
                 <div class="container">
                     <h4><b>John Doe</b></h4>
                     <p>Architect Engineer</p>
                 </div>
             </div>
             <div class="card">
-                <img style={{ width: '100%' }} />
+                <img src={process.env.PUBLIC_URL + "images/RPSLS.png"} />
                 <div class="container">
                     <h4><b>John Doe</b></h4>
                     <p>Architect Engineer</p>
