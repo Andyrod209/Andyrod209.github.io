06755a01bdc2500b7665abf40f29b477e4becc91
andyrod209 <andyrod209@gmail.com>
2022-05-05 16:35:39 -0500

    Incomplete: finish styling footer.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,22 +2,19 @@ import './Footer.css'
 
 const Footer = () => {
     return ( 
-        <div>
-            <h1>Contact Me</h1>
-            <div className='ways-to-contact'>
-                <ul>
-                    {/* 
-                    added all socails and emails. 
-                    make contact info stay at the bottom 
-                    at all times 
-                    */}
-                    <li><b>phone:</b>(209) 693-1596</li>
-                    <li><b>Linked In</b></li>
-                    <li><b></b></li>
-                    <li><b></b></li>
-                </ul>
+        <ul>
+            <div className='list'>
+                {/* 
+                added all socails and emails. 
+                make contact info stay at the bottom 
+                at all times 
+                */}
+                <li><b>Linked In</b></li>
+                <li><b>GitHub</b></li>
+                <li><b>Email:</b> rodr.andye@gmail.com</li>
+                <li><b>phone:</b> (209) 693-1596</li>
             </div>
-        </div>
+        </ul>
      );
 }
  
