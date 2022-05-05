5841b477bb48f21019862a144f9954d4270eb4c0
andyrod209 <andyrod209@gmail.com>
2022-05-05 17:13:58 -0500

    Chore: Added a tags for links.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,19 +2,19 @@ import './Footer.css'
 
 const Footer = () => {
     return ( 
-        <ul>
-            <div className='list'>
+        <div >
+        <ul className='list'>
                 {/* 
                 added all socails and emails. 
                 make contact info stay at the bottom 
                 at all times 
                 */}
-                <li><b>Linked In</b></li>
-                <li><b>GitHub</b></li>
-                <li><b>Email:</b> rodr.andye@gmail.com</li>
+                <li><a href='https://www.linkedin.com/in/andrew-e-rodriguez/' target="_blank"><b>Linked In</b></a></li>
+                <li><a href=''><b>GitHub</b></a></li>
+                <li><a href=''><b>Email:</b> rodr.andye@gmail.com</a></li>
                 <li><b>phone:</b> (209) 693-1596</li>
-            </div>
         </ul>
+        </div>
      );
 }
  
