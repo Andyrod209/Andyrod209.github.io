49941b288c5f8b34c1662b7faa76c20630468191
andyrod209 <andyrod209@gmail.com>
2022-05-05 23:43:54 -0500

    Incomplete working on navbar check comment on Navbar.css file

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,8 +5,7 @@ const HomePage = () => {
 
     return ( 
         <div className="home">
-          <h1>Hello! I'm a <u>Full Stack Developer!</u></h1>
-                
+          <h1>Hello! I'm a <u>Full Stack Developer!</u></h1>   
             <p class="home-text">My name is Andrew Rodriguez and I am a software developer!</p>
             <form action={process.env.PUBLIC_URL + "extraFiles/Rodriguez_Resume.pdf"} target='_blank'>
               <button type="submit">Resume</button>
