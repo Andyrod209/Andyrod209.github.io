aecef6b9782353a99bce5eb21d763e99ffce740a
andyrod209 <andyrod209@gmail.com>
2022-05-05 23:08:28 -0500

    Minor changes...

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -8,8 +8,9 @@ const HomePage = () => {
           <h1>Hello! I'm a <u>Full Stack Developer!</u></h1>
                 
             <p class="home-text">My name is Andrew Rodriguez and I am a software developer!</p>
-
-            <button><a href={process.env.PUBLIC_URL + "extraFiles/Rodriguez_Resume.pdf"} target="_blank">Resume</a></button>
+            <form action={process.env.PUBLIC_URL + "extraFiles/Rodriguez_Resume.pdf"} target='_blank'>
+              <button type="submit">Resume</button>
+            </form>
         </div> );
 }
  
