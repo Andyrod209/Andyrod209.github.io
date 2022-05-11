09d270f5548428bba898de3986670ca13591d99d
andyrod209 <andyrod209@gmail.com>
2022-05-10 22:44:56 -0500

    Refactor: Navbar is changed with name to being on the left and content to the right.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import './Navbar.css'
 const Navbar = () => {
     return ( 
-        <nav className="navbar">
-            <ul>
-                <li ><a href='/'>Andrew Rodriguez</a></li>
+        <nav >
+            <ul className="navbar">
+                <li className='name'><a href='/'>Andrew Rodriguez</a></li>
                 <li ><a href="/about">About</a></li>
                 <li ><a href='/projects'>Projects</a></li>
             </ul>
