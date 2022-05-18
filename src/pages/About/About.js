e519bc2f4d69aa1a46727ad1245fd573c948d288
andyrod <andyrod209@gmail.com>
2022-05-17 22:31:57 -0700

    Chore: Added a About me title on About me Section.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,12 +7,15 @@ const About = () => {
             <img className="image-of-andrew" src={process.env.PUBLIC_URL+"images/pic-of-andrew-rodriguez.jpg"} 
                 alt="Picture of Andrew Rodriguez"/>
 
-            <span><p>Hello I am Andrew Rodriguez a Full Stack Software Developer. I can build all types of
-            websites if you need me to! I can study and pull data from a database. I can do anything 
-            anything in this field. I just need a company to build more experience. Trust me I will be 
-            your best candidate for any coding related job becuase I'll be getting better and better at it!
-            Hire me!
-            </p></span>
+            <span>
+                <h4>About Me</h4>
+                <p>Hello I am Andrew Rodriguez a Full Stack Software Developer. I can build all types of
+                websites if you need me to! I can study and pull data from a database. I can do anything 
+                anything in this field. I just need a company to build more experience. Trust me I will be 
+                your best candidate for any coding related job becuase I'll be getting better and better at it!
+                Hire me!
+                </p>
+            </span>
         </div> );
 }
  
