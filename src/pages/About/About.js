ab5dfb023c348ca7de99d82423b21cdc2842ed36
andyrod <andyrod209@gmail.com>
2022-05-17 22:52:26 -0700

    Chore: Certificate is on About me section for user to be able to click.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -9,11 +9,9 @@ const About = () => {
 
             <span>
                 <h4>About Me</h4>
-                <p>Hello I am Andrew Rodriguez a Full Stack Software Developer. I can build all types of
-                websites if you need me to! I can study and pull data from a database. I can do anything 
-                anything in this field. I just need a company to build more experience. Trust me I will be 
-                your best candidate for any coding related job becuase I'll be getting better and better at it!
-                Hire me!
+                <p>Hello I am Andrew Rodriguez a Full Stack Software Developer. I went through a fast paced 
+                    14-week full stack course, now completed ( You can find my certification of completion 
+                    <a href={process.env.PUBLIC_URL + "extraFiles/AndrewRodriguezCertificate.pdf"} target="_blank"> here</a> ). 
                 </p>
             </span>
         </div> );
